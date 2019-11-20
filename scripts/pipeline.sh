ca7780510d2d2701e55c49deecbe56b0dc224e89
Hugo Bronchalo Bautista <hugo23796@gmail.com>
2019-11-20 20:28:49 +0100

    Final pipeline scipt

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -46,8 +46,14 @@ done
 # TODO: create a log file containing information from cutadapt and star logs
 for sid in $(ls log/cutadapt/*.log | cut -d"." -f1 | sed 's:log/cutadapt/::' | sort)
 do
-        cat log/cutadapt/${sid}.log | grep "Reads with adapters" >> log/pipeline.log
+        echo >> log/pipeline.log
+	echo "${sid}" >> log/pipeline.log
+	echo >> log/pipeline.log
+	echo "CUTADAPT ANALYSE FOR ${sid} :" >> log/pipeline.log
+	cat log/cutadapt/${sid}.log | grep "Reads with adapters" >> log/pipeline.log
         cat log/cutadapt/${sid}.log | grep "Total basepair" >> log/pipeline.log
+	echo >> log/pipeline.log
+	echo "STAR ANALYSE FOR ${sid} :" >> log/pipeline.log
         cat out/star/${sid}/Log.final.out | grep "Uniquely mapped reads %" >> log/pipeline.log
         cat out/star/${sid}/Log.final.out | grep "% of reads mapped to multiple loci" >> log/pipeline.log
         cat out/star/${sid}/Log.final.out | grep "% of reads mapped to too many loci" >> log/pipeline.log
