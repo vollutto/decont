241344d6cbaab808ca02678906945cf9af89ae02
Hugo Bronchalo Bautista <hugo23796@gmail.com>
2019-11-21 19:19:01 +0100

    Final Script with the Bonus Exercise

--- scripts/bonus_pipeline.sh
+++ scripts/bonus_pipeline.sh
@@ -2,23 +2,11 @@ cd /home/user05/DECONT/decont
 export WD=$(pwd)
 
 #Download all the files specified in data/urls
-echo "Downloading the files..."
-for url in $(cat data/urls) #TODO
-do
-
-    bash scripts/download.sh $url data
-done
-
-if [ -f "data/*.fastq.gz" ]
-then
-	echo "Already downloaded the files"
-else
-	echo "Downloading the files..."
-	wget -i data/urls -P data/
-fi
+echo "Downloading the data files ..."
+	wget -nc -i data/urls -P data/
 
 # Download the contaminants fasta file, and uncompress it
-if [ -f "res/*.fasta.gz" ]
+if [ -f "res/contaminants.fasta.gz" ]
 then
 	echo "Already downloaded the contaminants fasta files"
 else
@@ -42,7 +30,7 @@ for sid in $(ls data/*.fastq.gz | cut -d"-" -f1 | sed 's:data/::' | sort | uniq)
 do
 	if [ -f "out/merged/${sid}.fastq.gz" ]
 	then
-		echo "Already merged the samples into a single file"
+		echo "Already merged the samples of $sid into a single file"
 	else
 		echo "Running Merge..."
    		bash scripts/merge_fastqs.sh data out/merged $sid
@@ -54,9 +42,9 @@ mkdir -p out/trimmed
 mkdir -p log/cutadapt
 for sampleid in $(ls out/merged/*.fastq.gz | cut -d"." -f1 | sed 's:out/merged/::' | sort)
 do
-	if [ -f "out/trimmed/${sid}.trimmed.fastq.gz" ]
+	if [ -f "out/trimmed/${sampleid}.trimmed.fastq.gz" ]
 	then
-		echo "Already run the cutadapt for all merged files"
+		echo "Already run the cutadapt for $sampleid merged files"
 	else
 		echo "Running cutadapt..."
 		cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o out/trimmed/${sampleid}.trimmed.fastq.gz out/merged/${sampleid}.fastq.gz > log/cutadapt/${sampleid}.log
@@ -69,12 +57,12 @@ do
 	# you will need to obtain the sample ID from the filename
 	sid=$(basename $fname .trimmed.fastq.gz)
 	mkdir -p out/star/$sid
-	if [ -f "out/star/$s{sid}/Log.final.out" ]
+	if [ -f "out/star/$sid/Log.final.out" ]
 	then
-		echo "Already run STAR for all trimmed files"
+		echo "Already run STAR for $sid trimmed files"
 	else
 		echo "Running STAR..."
-    		STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn out/trimmed/${sid}.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix out/star/${sid}/
+    		STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn out/trimmed/$sid.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix out/star/${sid}/
 	fi
 done
 
